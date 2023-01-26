8a4cc6d0ed4c1d919936f40d10e8aaa205097e96
Josh Butts <josh.butts@ziffmedia.com>
2023-01-26 17:09:46 -0500

    add primay key and incrementing

--- src/Uuid62/Concerns/HasUuid62.php
+++ src/Uuid62/Concerns/HasUuid62.php
@@ -6,13 +6,20 @@ use ZiffDavis\Uuid62\Uuid62;
 
 trait HasUuid62
 {
-    public static function bootUuidable()
+    public static function bootHasUuid62()
     {
         static::saving(function ($model) {
             if (!isset($model->uuid)) {
                 $model->uuid = Uuid62::new();
             }
         });
+
+    }
+
+    public function initializeHasUuid62()
+    {
+        $this->incrementing = false;
+        $this->primaryKey = 'uuid';
     }
 
     public function scopeWithUuid(Builder $query, $uuid)
